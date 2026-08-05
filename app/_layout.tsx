211921d1022ecd76dee89fb931e0242d5513ca8f
thenitrodad <thenitrodad@gmail.com>
2026-08-05 02:20:04 +0000

    fix: remove react-native-url-polyfill (breaks Babel on EAS), drop polyfill import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,3 @@
-import 'react-native-url-polyfill/auto';
 import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
