a98c37d3bb264fbf044d284e74a1bc658ed513f4
thenitrodad <thenitrodad@gmail.com>
2026-08-05 02:33:45 +0000

    fix: force-transform @supabase packages through Babel in Metro config

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,28 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Supabase v2 uses package.exports which some Metro versions don't handle.
-// Disabling unstable_enablePackageExports forces classic main/module resolution.
+// Disable package.exports to avoid ESM resolution issues with Supabase
 config.resolver.unstable_enablePackageExports = false;
+
+// Force Metro to transform @supabase packages through Babel
+// (some Supabase sub-packages ship ES module syntax that Metro can't parse natively)
+config.transformer.transformIgnorePatterns = [
+  'node_modules/(?!(' +
+    [
+      'react-native',
+      '@react-native',
+      '@react-native-community',
+      'expo',
+      '@expo',
+      'expo-router',
+      '@supabase',
+      'react-native-url-polyfill',
+      'react-native-reanimated',
+      'react-native-gesture-handler',
+      'react-native-screens',
+      'react-native-safe-area-context',
+    ].join('|') +
+  ')/)',
+];
 
 module.exports = config;
